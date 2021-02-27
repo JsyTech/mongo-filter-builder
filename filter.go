8efad45d6de2c99695a7a751973fbc16ea673c87
Kend <kendsheld@outlook.com>
2021-02-28 00:55:26 +0800

    add operator constants

---
+++ filter.go
@@ -0,0 +1,13 @@
+package filter
+
+const (
+	_eq    = "$eq"
+	_ne    = "$ne"
+	_gt    = "$gt"
+	_gte   = "$gte"
+	_lt    = "$lt"
+	_lte   = "$lte"
+	_in    = "$in"
+	_nin   = "nin"
+	_regex = "$regex"
+)
